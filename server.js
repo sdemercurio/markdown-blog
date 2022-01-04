bcbed0e25df228f3f2b30d12003a8b5e5e5d71c7
Sarah DeMercurio <sdemercuriomi@gmail.com>
2022-01-04 12:03:43 -0500

    establishing routes

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const postRouter = require('./routes/posts');
 const app = express();
 
 app.set('view engine', 'ejs')
 
+app.use('/posts', postRouter)
+
 app.get('/', (req, res) => {
-    res.render('index')
+    const posts = [{
+        title: 'Test Post',
+        dateCreated: Date.now(),
+        description: 'Test description'
+    }]
+    res.render('index', { posts: posts })
 })
 
 app.listen(5000)
